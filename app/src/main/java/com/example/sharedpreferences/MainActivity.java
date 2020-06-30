cc4995d0dad5c90b54cf941b028dcc4583245b8e
erkin <ax.erkin@gmail.com>
2020-06-30 19:41:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sharedpreferences;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences(
+                "mySharedPreferences", Context.MODE_PRIVATE
+        );
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        //editor.putString("newHelloText", "Hello Shared Preferences");
+        editor.clear();
+        editor.apply();
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(sharedPreferences.getString("newHelloText",
+                "Default text"));
+    }
+}
